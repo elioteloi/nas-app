048284cdeeb84860ffc38fc0e3e20908ede4d0b2
Eliot Eloi <eliot.eloi22@gmail.com>
2025-01-31 13:28:28 -0300

    feat: add FileScreen to display image from server

---
+++ src/screens/FileScreen.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Config from "react-native-config";
+import { StyleSheet, Image, View } from 'react-native';
+
+const FileScreen = ({ route }) => {
+
+    const {userdrive, folder, filename} = route.params
+
+    console.log("File Screen", filename);
+    
+    return (
+        <View>
+            <Image 
+              source={{ uri: `http://${Config.API_IP_ADDRESS}:3000/path_Of_Drive/${userdrive}/${folder}/${filename}`}}
+              style={styles.picture} 
+              resizeMode="contain" 
+              />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    picture: {
+      width: "100%",
+      height: "100%"
+    },
+  });
+  
+
+
+export default FileScreen
