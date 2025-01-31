746d90c5fc15874a140633d9f556ff0c484c08e4
Eliot Eloi <eliot.eloi22@gmail.com>
2025-01-31 13:21:16 -0300

    feat: add AppNavigation to the main App component

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AppNavigation from './src/navigation/AppNavigation';
+
+const App = () => {
+
+  return (
+    <>
+      <AppNavigation/>
+    </>
+  );
+};
+
+export default App;
