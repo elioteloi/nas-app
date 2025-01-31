b5e332eb0a0da7dceea27d551cdba39f768525bd
Eliot Eloi <eliot.eloi22@gmail.com>
2025-01-31 13:51:22 -0300

    feat: add reusable Button component with customizable style and behavior

---
+++ src/components/Button.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { TouchableOpacity, Text, StyleSheet } from "react-native"
+
+const Button = ({title, onPress, backgroundColor}) => {
+    return (
+        <TouchableOpacity style={[styles.button, {backgroundColor}]} onPress={onPress}>
+            <Text style={styles.TextButton}>{title}</Text>
+        </TouchableOpacity>
+
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        padding: 10,
+        margin: 10,
+        borderRadius: 5,
+        alignItems: 'center',
+      },
+      TextButton: {
+        color: "#F5F5F5",
+      },
+})
+export default Button;
