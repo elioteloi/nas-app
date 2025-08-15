74b832b18165da78b4435af418ea4e271059c986
Eliot Eloi <eliot.eloi22@gmail.com>
2025-08-15 18:18:37 -0300

    feat: add change password components in the profile screen

--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -1,38 +1,51 @@
 import React, { useContext, useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import AuthContext from "../context/AuthContext";
-import Button from "../components/Button";
 import userApi from "../api/userApi";
-import Input from "../components/Input";
-import TextProfile from "../components/Text";
 import ButtonModal from "../components/ButtonModal";
-import ChangePassword from "../components/ChangePassword";
+import TextProfile from "../components/Text";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import TextError from "../components/TextError";
+import TextSuccess from "../components/TextSuccess";
 
 const ProfileScreen = () => {
 const [currentPassword, onChangeText] = useState('')
 const [newPassword, setNewPassword] = useState('')
+const [errorMessage, setErrorMessage] = useState('')
+const [successMessage, setSuccessMessage] = useState('')
 
-const { id, name, email, logout } = useContext(AuthContext)
-const { updateUser, deleteUser } = userApi()
+const { id, name, email, logout, deleteAccount } = useContext(AuthContext)
+const { updateUser } = userApi()
 
-  const handleUpdate = (id, currentPassword, newPassword) => {
-    updateUser(id, currentPassword, newPassword)
-  }
 
-  const handleDelete = (id) => {
-    logout
-    deleteUser(id)
-  }
+
     return (
         <View style={styles.container}>
-            <ChangePassword id={id} name={name} email={email} triggerFunction={handleUpdate}/>
+          <TextProfile content={`Welcome ${name}`}/>
+            <TextProfile content={`Email: ${email}`}/>
+            <Input placeholder='Current password' onChangeText={onChangeText} value={currentPassword}/>
+            {errorMessage ? <TextError errorMessage={errorMessage}/> : null}
+            {successMessage ? <TextSuccess successMessage={successMessage}/> : null}
+
+            <Input placeholder='New password'onChangeText={setNewPassword} value={newPassword}/>
+            <Button title="update password" onPress={async () => {
+               json = await updateUser(id, currentPassword, newPassword)
+               console.log(json);
+               {json.message ? setSuccessMessage(json.message): setErrorMessage(json.error)}
+               
+                
+            }} backgroundColor="#CD5C5C"/>
+
             <View style={styles.bottomContainer}>
 
 
-                  <ButtonModal title="logged out" textAlert="Are you sure that you want to logout ?" titleBtnModal="logged out" onPress={async () => {
+                  <ButtonModal title="Logged out" textAlert="Are you sure that you want to logout ?" titleBtnModal="logged out" onPress={async () => {
                     await logout()
                   }}/>
-                  <ButtonModal title="Delete acccount" textAlert="Are you sure that you want to delete the account" titleBtnModal="Delete account" id={id} triggerFunction={handleDelete}/>
+                  <ButtonModal title="Delete acccount" textAlert="Are you sure that you want to delete the account" titleBtnModal="Delete account" onPress={async () => {
+                    await deleteAccount(id)
+                  }} />
               
             </View>
         </View>
