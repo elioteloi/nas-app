74109b91355772fffc1b0ba896aae0aca758014a
Eliot Eloi <eliot.eloi22@gmail.com>
2025-10-26 21:26:22 -0300

    feat: add path to port

--- src/components/File.jsx
+++ src/components/File.jsx
@@ -15,7 +15,7 @@ const File = ({ item, folder }) => {
                 filename: item.filename,
                 originalname: item.originalname})}>
             <Image 
-                source={{ uri: `http://${Config.API_IP_ADDRESS}:3000/path_Of_Drive/${item.userdrive}/${folder}/${item.filename}`}}
+                source={{ uri: `http://${Config.API_IP_ADDRESS}:${Config.PORT}/path_Of_Drive/${item.userdrive}/${folder}/${item.filename}`}}
                 style={{ width: 120, height: 120 }}
                 onError={(error) => console.error('Image loading error:', error.nativeEvent.error)}
             />
