2fbd721b853f4b6570d7234ec68c9e438f0ddd74
Eliot Eloi <eliot.eloi22@gmail.com>
2025-10-26 21:28:00 -0300

    feat: add .env variable to path

---
+++ src/api/syncApi.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Config from "react-native-config";
+
+const syncApi = () => {
+        const createSync = async (file) => {
+          try {
+            const response = await fetch(`http://${Config.API_IP_ADDRESS}:${Config.PORT}/sync`, {
+                method: 'POST',
+               
+                body: file
+              })
+                  
+            let data = await response.json();
+            return data;
+            
+          } catch (error) {
+            console.error('Error syncing file:', error);
+            throw error;
+          }  
+    }
+
+        const fetchSync = async (id) => {
+          try {
+            const response = await fetch(`http://${Config.API_IP_ADDRESS}:${Config.PORT}/fetchSync`, {
+              method: "POST",
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({id})
+            })
+
+            let data = await response.json()
+            return data
+          } catch (error) {
+            console.error('Error fetching syncing file:', error);
+            throw error;
+          }
+        }
+
+    return { createSync, fetchSync }
+} 
+
+export default syncApi;
+
