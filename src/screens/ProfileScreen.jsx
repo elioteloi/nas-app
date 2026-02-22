08cf449a73c64208a68bda52a4ce6e84baa6f366
eliot eloi <eliot.eloi22@gmail.com>
2026-02-22 19:42:11 -0300

    feat: add the switch and folders to choose the folders to sync and send them via asyncStorage

--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View, NativeModules, Image, Text, Switch, ScrollView } from "react-native";
 import AuthContext from "../context/AuthContext";
 import userApi from "../api/userApi";
 import ButtonModal from "../components/ButtonModal";
@@ -8,6 +8,8 @@ import Input from "../components/Input";
 import Button from "../components/Button";
 import TextError from "../components/TextError";
 import TextSuccess from "../components/TextSuccess";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+const { MediaStoreModule } = NativeModules;
 
 const ProfileScreen = () => {
 const [currentPassword, onChangeText] = useState('')
@@ -15,15 +17,91 @@ const [newPassword, setNewPassword] = useState('')
 const [errorMessage, setErrorMessage] = useState('')
 const [successMessage, setSuccessMessage] = useState('')
 
-const { id, name, email, logout, deleteAccount } = useContext(AuthContext)
+const { id, name, email, logout, deleteAccount, folderSync } = useContext(AuthContext)
 const { updateUser } = userApi()
 
+const [folderSwitch, setFolderSwitch] = useState()
+
+   async function fetchFileFunc() {
+      // await AsyncStorage.removeItem('folderSync');
+
+            const values = await AsyncStorage.getItem('folderSync')
+            const sync = JSON.parse(values);
+
+          console.log("asyncStorage ", sync);
+
+          const folders = await MediaStoreModule.getImageFolders();
+
+      setFolderSwitch(sync)
+
+    }
+
+  const toggleSwitch = (switchs, value) => {
+    console.log("Full item data:", switchs);
+    console.log("asyncStorage:", folderSwitch);
+    const result = folderSwitch.map((item, index) => {
+
+    if (item.id === switchs.id) {
+      return {...item, boolean: value}
+    }
+    return item
+});
+
+    setFolderSwitch(result)
+    folderSync(result)
+
+console.log("result: ", result);
+
+    
+  };
+
+  useEffect(() => {   
+    
+    fetchFileFunc()
+  }, [])
 
 
     return (
-        <View style={styles.container}>
+        <ScrollView style={styles.container}>
           <TextProfile content={`Welcome ${name}`}/>
             <TextProfile content={`Email: ${email}`}/>
+            <View>
+            <FlatList
+                    data={folderSwitch}
+                    keyExtractor={item => item.name}
+                    numColumns={1}
+            
+                     renderItem={({ item }) => (
+                      <View style={{ flexDirection: "row", padding: 5}}>
+                        <View style={{ padding: 10 }}>
+                            <Image
+                              key={item.id}
+                              source={{ uri: item.lastImage }}
+                              style={{ width: 110, height: 110, borderRadius: 8}}
+            
+                            />
+                        </View>
+                        <View style={{ display: "flex", justifyContent: "center"}}>
+                          <Text style={{ color: "black", alignContent: "center", justifyContent: "center", fontSize: 18}}>{item.name}</Text>
+                          <Text style={{ color: "black", alignContent: "center", justifyContent: "center"}}>{item.count} Photos</Text>
+            
+                        </View>
+            
+                        <View style={{ marginLeft: "auto", justifyContent: "center" }}>
+                            <Switch
+                              trackColor={{false: '#767577', true: '#81b0ff'}}
+                              thumbColor={item.boolean ? '#1362eb' : '#f4f3f4'}
+                              ios_backgroundColor="#3e3e3e"
+                              onValueChange={(value) => {toggleSwitch(item, value)}}
+                              value={item.boolean}
+                            />
+                        </View>
+                    </View>
+                    )}
+            
+                  />
+                  
+                </View>
             <Input placeholder='Current password' onChangeText={onChangeText} value={currentPassword}/>
             {errorMessage ? <TextError errorMessage={errorMessage}/> : null}
             {successMessage ? <TextSuccess successMessage={successMessage}/> : null}
@@ -36,7 +114,7 @@ const { updateUser } = userApi()
                
                 
             }} backgroundColor="#CD5C5C"/>
-
+            
             <View style={styles.bottomContainer}>
 
 
@@ -48,7 +126,7 @@ const { updateUser } = userApi()
                   }} />
               
             </View>
-        </View>
+        </ScrollView>
     )
 }
 
@@ -60,6 +138,7 @@ const styles = StyleSheet.create({
     bottomContainer: {
       flex: 1,
       justifyContent: 'flex-end',
+      paddingBottom: 20
     },
   });
 
