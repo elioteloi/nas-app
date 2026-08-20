4f4c16baa8389ae454a0615132111a5d26163a65
Eliot Eloi <eliot.eloi22@gmail.com>
2026-08-20 13:35:13 -0300

    feat: add url screen and change params to stack screens

--- src/navigation/AppNavigation.jsx
+++ src/navigation/AppNavigation.jsx
@@ -1,46 +1,47 @@
-import React, { useContext } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React, {useContext} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import LoginScreen from '../screens/LoginScreen';
 import SigninScreen from '../screens/SigninScreen';
-import AuthContext, { AuthProvider } from '../context/AuthContext';
+import AuthContext, {AuthProvider} from '../context/AuthContext';
 import FolderScreen from '../screens/FolderScreen';
 import FileScreen from '../screens/FileScreen';
 import PictureScreen from '../screens/PictureScreen';
 import ProfileScreen from '../screens/ProfileScreen';
-import { Image } from 'react-native';
+import ServerUrlScreen from '../screens/ServerUrlScreen';
+import {Image} from 'react-native';
 
 const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
 const AppStack = () => {
-  const { isLoggedIn } = useContext(AuthContext);
+  const {isLoggedIn} = useContext(AuthContext);
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='signin'>
+      <Stack.Navigator initialRouteName="serverURl">
         {isLoggedIn ? (
           <>
-          <Stack.Screen
-            name="Tab"
-            component={AppTab}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="Folder"
-            component={FolderScreen}
-            options={({ route }) => ({
-              title: route.params?.folder || 'Folder',
-            })}
-          />
-          <Stack.Screen
-            name="File" 
-            component={FileScreen}
-            options={({ route }) => ({
-              title: route.params?.originalname || 'File',
-            })}
+            <Stack.Screen
+              name="Tab"
+              component={AppTab}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Folder"
+              component={FolderScreen}
+              options={({route}) => ({
+                title: route.params?.params || 'Folder',
+              })}
+            />
+            <Stack.Screen
+              name="File"
+              component={FileScreen}
+              options={({route}) => ({
+                title: route.params?.params[2] || 'File',
+              })}
             />
           </>
         ) : (
@@ -48,12 +49,17 @@ const AppStack = () => {
             <Stack.Screen
               name="signin"
               component={SigninScreen}
-              options={{ headerShown: false }}
+              options={{headerShown: false}}
             />
             <Stack.Screen
               name="login"
               component={LoginScreen}
-              options={{ headerShown: false }}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="serverURl"
+              component={ServerUrlScreen}
+              options={{headerShown: false}}
             />
           </>
         )}
@@ -65,45 +71,66 @@ const AppStack = () => {
 const AppTab = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name='Home' component={HomeScreen}
-      options={{
-        headerShown: false,
-        tabBarIcon: ({size, focused, color}) => {
-          return (
-            <Image
-            style={{ width: size, height: size, tintColor: focused ? '#0099ff' : 'gray',}}
-            source={require('../../assets/images/folder.png')}
-            />
-          )
-        }
-      }}/>
-      <Tab.Screen name='Pictures' component={PictureScreen}
-      options={{
-        headerShown: false,
-        tabBarIcon: ({size, focused, color}) => {
-          return (
-            <Image
-            style={{ width: size, height: size, tintColor: focused ? '#0099ff' : 'gray',}}
-            source={require('../../assets/images/photo.png')}
-            />
-          )
-        }
-      }}/>
-      <Tab.Screen name='Profile' component={ProfileScreen} 
-      options={{
-        headerShown: false,
-        tabBarIcon: ({size, focused, color}) => {
-          return (
-            <Image
-            style={{ width: size, height: size, tintColor: focused ? '#0099ff' : 'gray',}}
-            source={require('../../assets/images/user.png')}
-            />
-          )
-        }
-      }}/>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({size, focused, color}) => {
+            return (
+              <Image
+                style={{
+                  width: size,
+                  height: size,
+                  tintColor: focused ? '#0099ff' : 'gray',
+                }}
+                source={require('../../assets/images/folder.png')}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Pictures"
+        component={PictureScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({size, focused, color}) => {
+            return (
+              <Image
+                style={{
+                  width: size,
+                  height: size,
+                  tintColor: focused ? '#0099ff' : 'gray',
+                }}
+                source={require('../../assets/images/photo.png')}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({size, focused, color}) => {
+            return (
+              <Image
+                style={{
+                  width: size,
+                  height: size,
+                  tintColor: focused ? '#0099ff' : 'gray',
+                }}
+                source={require('../../assets/images/user.png')}
+              />
+            );
+          },
+        }}
+      />
     </Tab.Navigator>
-  )
-}
+  );
+};
 
 const AppNavigation = () => {
   return (
