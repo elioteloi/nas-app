12e2341a58ba592b1cd4ccefb840fdb2afa8b8c0
Eliot Eloi <eliot.eloi22@gmail.com>
2026-08-20 13:31:57 -0300

    feat: create a unique component Card

---
+++ src/components/Card.jsx
@@ -0,0 +1,107 @@
+import {useNavigation} from '@react-navigation/native';
+import React, {useState} from 'react';
+import {Image, Pressable, StyleSheet, Text, View} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import Button from './Button';
+import BottomSheet from './BottomSheet';
+import ButtonModal from './ButtonModal';
+import Input from './Input';
+import folderIcon from '../../assets/images/folder.png';
+import folderApi from '../api/folderApi';
+
+const Card = ({
+  navigationPage,
+  paramsNavigation,
+  titleBottomSheet,
+  iconBottomSheet,
+  onPressUpdate,
+  onPressDelete,
+  imageCard,
+  iconButtonBottomSheet,
+  titleCard,
+}) => {
+  const navigation = useNavigation();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [changeText, setChangeText] = useState('');
+
+  const {fetchFolder, updateFolder, deleteFolder} = folderApi();
+
+  return (
+    <>
+      <Pressable
+        style={styles.item}
+        onPress={() =>
+          navigation.navigate(navigationPage, {params: paramsNavigation})
+        }>
+        <View style={styles.optionButton}>
+          <BottomSheet
+            title={titleBottomSheet}
+            iconBottomSheet={imageCard}
+            iconButtonBottomSheet={iconButtonBottomSheet}>
+            <ButtonModal
+              title="Update"
+              titleBtnModal="Update folder"
+              onPress={onPressUpdate}>
+              <Input
+                placeholder="Change the name for the folder"
+                onChangeText={setChangeText}
+                value={changeText}
+              />
+            </ButtonModal>
+
+            <ButtonModal
+              title="Delete"
+              textAlert="Are you sure that you want to delete the folder !"
+              titleBtnModal="Delete folder"
+              onPress={onPressDelete}
+            />
+          </BottomSheet>
+        </View>
+
+        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+          <Image source={imageCard} style={styles.image} />
+        </View>
+      </Pressable>
+      <Text style={styles.text}>{titleCard}</Text>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    height: 100,
+    width: 190,
+    margin: '1%',
+    padding: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 10,
+  },
+
+  optionButton: {
+    position: 'absolute',
+    top: 5,
+    right: 5,
+  },
+  image: {
+    width: 60,
+    height: 60,
+  },
+
+  text: {
+    color: 'black',
+  },
+  buttonContainer: {
+    padding: 8,
+  },
+
+  button: {
+    width: 25,
+    height: 25,
+  },
+});
+
+export default Card;
